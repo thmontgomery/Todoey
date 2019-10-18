c037dbb98a1a62c551f24f8e9f02466c935ea257
thmontgomery <tmontgomery@bama.ua.edu>
2019-10-18 12:37:08 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Taylor Montgomery on 10/17/19.
+//  Copyright © 2019 Taylor Montgomery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
